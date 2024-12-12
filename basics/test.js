75962fa141145138a3f8ec4aa226bbeefe551367
Dipanwita Bala <106340756+Dip-Bala@users.noreply.github.com>
2024-12-12 11:17:20 +0000

    basics complete

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,29 @@
 console.log("Dipanwita")
+const password = 3456789
+// password = 36546787 
+var id = 755768
+// console.log(password);
+let email = "dip@gmail.com"
+// console.table([id, email, password])
+
+// const variables value is constant, block scope and can not the reassigned after and must be initialized at the time of declaration
+//let is block scoped, can be reassigned
+// function scoped(If declared inside a function, it's scoped to that function. If outside, it's scoped globally.)
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
