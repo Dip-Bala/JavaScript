a807c5b33bbd3ce156bf12108202735a4c69d516
Dipanwita Bala <106340756+Dip-Bala@users.noreply.github.com>
2025-03-31 16:41:32 +0000

    promisified fs created

--- medium/promises.js
+++ medium/promises.js
@@ -1,3 +1,9 @@
+// A Promise in JavaScript is an object that represents the eventual completion (or failure) of an asynchronous operation 
+// and its resulting value. Promises are used to handle asynchronous operations more effectively than traditional callback 
+// functions, providing a cleaner and more manageable way to deal with code that executes asynchronously,
+// such as API calls, file I/O, or timers.
+
+
 // Promisified version of setTimeout
 function random(resolve){
     // resolve(); // called instantly 
@@ -10,9 +16,61 @@ function main(){
 // p.then(main);
 
 // Now let's write everything in a complex & fancy way
-function setTimeoutProsified(ms){
+function setTimeoutPromisified(ms){
     return new Promise((resolve) => { //promise takes a func1 as argument --> func1 takes another func as argument and calls it after specified time
         setTimeout(resolve, ms);
     })
 }
-setTimeoutProsified(3000).then(main);
+function callback() {
+	console.log("3 seconds have passed");
+}
+
+// setTimeoutPromisified(3000).then(callback);
+
+/************************* File System Promisified versions************************************ */
+
+const fs = require('fs');
+// callback approach
+// function cleanFile(file, onDone){
+//     fs.readFile(file, 'utf-8', function(err, data){
+//         data = data.trim();
+//         fs.writeFile(file, data, onDone);
+//     })
+// }
+
+function onDone(){
+    console.log('File is cleaned');
+}
+// cleanFile("medium/a.txt", onDone);
+
+// Promisified Approach
+function cleanFile(filePath){
+    return new Promise(promisifiedReadFile);
+}
+function promisifiedReadFile(readTheFile, filePath){
+    readTheFile();
+}
+function readTheFile(resolve, filePath){
+    fs.readFile(file, 'utf-8', function(err, data){
+        data = data.trim();
+        fs.writeFile(file, data, resolve);
+    });
+}
+
+//In one function 
+function cleanFile(filePath){
+    return new Promise(
+        (done) => {
+            fs.readFile(filePath, 'utf-8', function(err, data){
+                data = data.trim();
+                fs.writeFile(filePath, data, function(){
+                    console.log(data);
+                    done();
+                });
+            });
+        }
+    );
+}
+
+cleanFile("medium/a.txt").then(onDone);
+
